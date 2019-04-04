0e4f5fa77c6357f098c5868faedfdf382ea44c49
unknown <sherman.jonathan.r@gmail.com>
2019-04-03 19:53:00 -0700

    adding weather api.js

---
+++ assets/firebase-search-results.js
@@ -0,0 +1,36 @@
+// var config = {
+//   apiKey: "AIzaSyAdbLOzyRP7TX1AZ5Ero01bzzYJaz4WmUs",
+//   authDomain: "project-6a7dc.firebaseapp.com",
+//   databaseURL: "https://project-6a7dc.firebaseio.com",
+//   projectId: "project-6a7dc",
+//   storageBucket: "project-6a7dc.appspot.com",
+//   messagingSenderId: "119882092763"
+// };
+
+// firebase.initializeApp(config);
+
+// var database = firebase.database();
+
+// $("#drift-button").on("click", function (event) {
+//   event.preventDefault();
+
+//   var city = $("#").val().trim();
+//   var state = $("#").val().trim();
+
+//   var recent = {
+//     city: city,
+//     state: state
+//   };
+
+//   database.ref().push(recent);
+
+// });
+
+// database.ref().on("child_added", function() {
+//   var city = childSnapshot.val().city;
+//   var state = childSnapshot.val().state;
+
+//   $("#city").html(city);
+//   $("#state").html(state);
+  
+// });
--- assets/firebase.js
+++ assets/firebase.js
@@ -1,36 +1,9 @@
 var config = {
-  apiKey: "AIzaSyAdbLOzyRP7TX1AZ5Ero01bzzYJaz4WmUs",
-  authDomain: "project-6a7dc.firebaseapp.com",
-  databaseURL: "https://project-6a7dc.firebaseio.com",
-  projectId: "project-6a7dc",
-  storageBucket: "project-6a7dc.appspot.com",
-  messagingSenderId: "119882092763"
+    apiKey: "AIzaSyBpcIJeopADYm46hNUATGvgdxVcS4hnyuE",
+    authDomain: "project1-41f6a.firebaseapp.com",
+    databaseURL: "https://project1-41f6a.firebaseio.com",
+    projectId: "project1-41f6a",
+    storageBucket: "project1-41f6a.appspot.com",
+    messagingSenderId: "79635430000"
 };
-
 firebase.initializeApp(config);
-
-var database = firebase.database();
-
-$("#drift-button").on("click", function (event) {
-  event.preventDefault();
-
-  var city = $("#").val().trim();
-  var state = $("#").val().trim();
-
-  var recent = {
-    city: city,
-    state: state
-  };
-
-  database.ref().push(recent);
-
-});
-
-database.ref().on("child_added", function() {
-  var city = childSnapshot.val().city;
-  var state = childSnapshot.val().state;
-
-  $("#city").html(city);
-  $("#state").html(state);
-  
-});
