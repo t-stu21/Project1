ab53fcf13e81b17cf11e6cc5b60ff9fc91e24fbc
unknown <sherman.jonathan.r@gmail.com>
2019-04-04 21:46:49 -0700

    firebase progress

--- assets/firebase-search-results.js
+++ assets/firebase-search-results.js
@@ -1,28 +1,43 @@
 var config = {
-    apiKey: "AIzaSyBYvs50zCq5GpVddL1ei3b_BPHu9B0Kmmk",
-    authDomain: "project-1-20b87.firebaseapp.com",
-    databaseURL: "https://project-1-20b87.firebaseio.com",
-    projectId: "project-1-20b87",
-    storageBucket: "project-1-20b87.appspot.com",
-    messagingSenderId: "242372759001"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyBYvs50zCq5GpVddL1ei3b_BPHu9B0Kmmk",
+  authDomain: "project-1-20b87.firebaseapp.com",
+  databaseURL: "https://project-1-20b87.firebaseio.com",
+  projectId: "project-1-20b87",
+  storageBucket: "project-1-20b87.appspot.com",
+  messagingSenderId: "242372759001"
+};
+firebase.initializeApp(config);
 
 var database = firebase.database();
 
-$("#drift-button").on("click", function () {
+$("#drift-button").on("click", function (event) {
 
-    var citySearched = $("#city").val().trim();
-    var stateSearched = $("#state").val().trim();
+  event.preventDefault();
 
-    console.log(citySearched);
-    console.log(stateSearched);
+  var citySearched = $("#city").val().trim();
+  var stateSearched = $("#state").val().trim();
 
-    var results = {
-        cityName: citySearched,
-        stateName: stateSearched,
-    };
+  // var locationRef = database.ref("top-five");
+  // locationRef.child('city').push(citySearched);
+  // locationRef.child('state').push(stateSearched);
 
-    database.ref().set(results);
+  console.log(citySearched);
+  console.log(stateSearched);
+
+  var results = {
+    city: citySearched,
+    state: stateSearched,
+  };
+
+  database.ref("searched").push(results);
+
+});
+
+database.ref("searched").on("child_added", function (childSnapshot) {
+  var city = childSnapshot.val().city;
+  var state = childSnapshot.val().state;
+
+// results id is where firebase data appends to
+    $("#results").html("City:  " + city + "  State:  " + state);
 
 });
